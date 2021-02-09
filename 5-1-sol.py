373c87299a010ec7ef88bc87465e63e2db2f7a45
Bae Jin Woo <jw969696@naver.com>
2021-02-09 19:15:43 +0900

    5장 python

---
+++ 5-1-sol.py
@@ -0,0 +1,37 @@
+# n, m = map(int, input().split())
+# print(n, m)
+
+# graph = []
+# visited = []
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+#     visited.append([0 for _ in range(m)])
+# print(visited)
+
+graph = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
+n, m = 4, 5
+visited = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+
+# 모든 노드에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
